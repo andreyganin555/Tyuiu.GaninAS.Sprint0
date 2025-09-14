653d10552c2bd04472f8f1ec9b343ad3c0b770f1
AndreyGanin <ganin25@inbox.ru>
2025-09-14 16:21:38 +0500

    Task4

---
+++ Tyuiu.GaninAS.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GaninAS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.GaninAS.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        public void CheckedSubtractionvalid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10,5));
+        }
+        public void CheckedMultiplicationvalid()
+        {
+            Assert.AreEqual(50,DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionvalid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+
+
+    }
+}
---
+++ Tyuiu.GaninAS.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GaninAS.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GaninAS.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ff643b4-7f9a-43d1-9a24-852c5e544c6b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
